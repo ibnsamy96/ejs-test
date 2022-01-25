39e8d19de3ff34217bb5b51be5def1282e342481
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-01-25 05:54:09 +0200

    removed unused code and added the whole project markup

--- server.js
+++ server.js
@@ -1,37 +1,19 @@
 const path = require("path");
 var express = require("express");
-var ejs = require("ejs");
 
 var app = express();
 
-// set the view engine to ejs
-// app.use("/public", express.static("public"));
-
 app.set("view engine", "ejs");
-app.use("/public", express.static(path.join(__dirname, "public")));
-console.log(path.join(__dirname, "styles"));
-app.use(express.json());
 
-// use res.render to load up an ejs view file
-let people = 10;
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
 // index page
 app.get("/", function (req, res) {
-  var mascots = [
-    { name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
-    { name: "Tux", organization: "Linux", birth_year: 1996 },
-    { name: "Moby Dock", organization: "Docker", birth_year: 2013 },
-  ];
-  var tagline =
-    "No programming concept is complete without a cute animal mascot.";
-
-  res.render("pages/index", {
-    mascots,
-    tagline,
-    people,
-  });
+  res.render("pages/index");
 });
 
+// method to get list html code
 app.get("/list-items", function (req, res) {
   itemsNumber = Number(req.query.items_number);
   if (itemsNumber > 0) {
